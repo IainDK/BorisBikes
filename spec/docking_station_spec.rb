bbcc3757aa876c01a6817caaf1b7395a35804d66
iaink <iainkinnish@googlemail.com>
2016-09-30 15:02:00 +0000

    Chapter 19 - started usingdoubles.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,45 +3,45 @@ require 'bike'
 
 describe DockingStation do
 
-  let(:bike) {double :bike }
+
 
 
   it { is_expected.to respond_to (:bike) }
 
   describe 'initialization' do
+    
+      #let(:Bike) { double :Bike }
+      
     it 'has default capacity' do
       expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
     end
-
+    
     it 'has a variable capacity' do
       docking_station = DockingStation.new(50)
-      50.times { docking_station.dock Bike.new }
-      docking_station
-      expect{ docking_station.dock Bike.new }.to raise_error 'Docking station full'
+      50.times { docking_station.dock Bike.new } # * double *
+      expect{ docking_station.dock Bike.new }.to raise_error 'Docking station full'   # * double *
     end
   end
 
 
   describe '#dock()' do
     it 'returns docked bikes' do
-      bike = Bike.new
+      bike = Bike.new                                       # * double * 
       subject.dock(bike)
       expect(subject.bike.pop).to eq bike
     end
 
     it 'returns error when docking station is full' do
-      #subject.dock(Bike.new)
-      bike = Bike.new
+      bike = Bike.new                                               # * double *
       DockingStation::DEFAULT_CAPACITY.times do
         subject.dock(bike)
-        #expect {subject.dock(bike)}.to raise_error 'Docking station at capacity'
       end
     end
 
     it 'accepts docking bikes, broken or not' do
-      bike = Bike.new
-      broken_bike = Bike.new
-      broken_bike.report_broken
+      bike = Bike.new                                   # * double *
+      broken_bike = Bike.new                            # * double *
+      broken_bike.report_broken                         # ~~ double ~~ 
       subject.dock(bike)
       subject.dock(broken_bike)
       expect(subject.bike.pop).to eq broken_bike
@@ -55,9 +55,9 @@ describe DockingStation do
     it { is_expected.to respond_to :release_bike }
 
     it 'releases working bikes' do
-      subject.dock(Bike.new)
+      subject.dock(Bike.new)                                              # * double *
       bike = subject.release_bike #FIX HERE
-      expect(bike).to be_working
+      expect(bike).to be_working                            # ~~ double ~~ 
     end
 
     it 'returns error when no bike available' do
@@ -67,8 +67,8 @@ describe DockingStation do
 
     it 'returns' do
       # I'd like docking stations not to release broken bikes.
-      broken_bike = Bike.new
-      broken_bike.report_broken
+      broken_bike = Bike.new                                                          # * double *
+      broken_bike.report_broken                                                       # ~~ double ~~
       subject.dock(broken_bike)
       expect { subject.release_bike }.to raise_error 'Cannot release. Bike broken'
     end
